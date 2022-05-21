77241ffdd18364620ea85f57e2249ab601193175
TJ-Abbing <tjabbing@gmail.com>
2022-05-21 15:34:47 +0200

    Added bubbbles, gave fishes randomly generated colors and both are animated

--- week-2/src/game.ts
+++ week-2/src/game.ts
@@ -1,43 +1,78 @@
-import * as PIXI from 'pixi.js'
+// imports
+import * as PIXI from "pixi.js"
 import fishImage from "./images/fish.png"
 import bubbleImage from "./images/bubble.png"
-import waterImage from "./images/water.jpg"
+import bgImage from "./images/water.jpg"
 
-//
-// STAP 1 - maak een pixi canvas
-//
-const pixi = new PIXI.Application({ width: 800, height: 450 })
-document.body.appendChild(pixi.view)
+export class Game { // puts entire game into game class
 
-//
-// STAP 2 - preload alle afbeeldingen
-//
-const loader = new PIXI.Loader()
-loader.add('fishTexture', fishImage)
-      .add('bubbleTexture', bubbleImage)
-      .add('waterTexture', waterImage)
-loader.load(()=>loadCompleted())
+    // states the classes within the game class
+    pixi: PIXI.Application
+    background:PIXI.Sprite
+    fish:PIXI.Sprite
+    anotherFish:PIXI.Sprite
+    bubble:PIXI.Sprite
+    anotherBubble:PIXI.Sprite
+    loader:PIXI.Loader
 
-//
-// STAP 3 - maak een sprite als de afbeeldingen zijn geladen
-//
-function loadCompleted() {
-    let water = new PIXI.Sprite(loader.resources["waterTexture"].texture!)
-    let bubble = new PIXI.Sprite(loader.resources["bubbleTexture"].texture!)
-    let fish = new PIXI.Sprite(loader.resources["fishTexture"].texture!)
-    pixi.stage.addChild(water)
+    constructor() { // constructor runs once class is called
+        this.pixi = new PIXI.Application({ width: 900, height: 500 })
+        document.body.appendChild(this.pixi.view)
 
-    bubble.x = 100
-    bubble.y = 200
-    pixi.stage.addChild(bubble)
+        this.loader = new PIXI.Loader()
+        this.loader
+            .add("fishTexture", fishImage)
+            .add("bubbleTexture", bubbleImage)
+            .add("backgroundTexture", bgImage)
 
-    fish.x = 100
-    fish.y = 200
-    pixi.stage.addChild(fish)
+        this.loader.load(() => this.doneLoading())
+    }
 
-    let anotherFish = new PIXI.Sprite(loader.resources["fishTexture"].texture!)
-    anotherFish.x = 400
-    anotherFish.y = 100
-    pixi.stage.addChild(anotherFish)
-    
+    doneLoading() {
+        console.log("all textures loaded!")
+
+        
+
+        // background
+        this.background = new PIXI.Sprite(this.loader.resources["backgroundTexture"].texture!)
+        this.pixi.stage.addChild(this.background)
+
+        // fish
+        this.fish = new PIXI.Sprite(this.loader.resources["fishTexture"].texture!)
+        this.pixi.stage.addChild(this.fish)
+        this.fish.tint = Math.random() * 0xFFFFFF;
+        this.fish.x = Math.floor(Math.random() * 800)
+        this.fish.y = Math.floor(Math.random() * 400)
+
+        // anotherFish
+        this.anotherFish = new PIXI.Sprite(this.loader.resources["fishTexture"].texture!)
+        this.pixi.stage.addChild(this.anotherFish)
+        this.anotherFish.tint = Math.random() * 0xFFFFFF;
+        this.anotherFish.x = Math.floor(Math.random() * 800)
+        this.anotherFish.y = Math.floor(Math.random() * 400)
+
+        // bubble
+        this.bubble = new PIXI.Sprite(this.loader.resources["bubbleTexture"].texture!)
+        this.pixi.stage.addChild(this.bubble)
+        this.bubble.x = Math.floor(Math.random() * 800)
+        this.bubble.y = Math.floor(Math.random() * 400)
+
+        // anotherBubble
+        this.anotherBubble = new PIXI.Sprite(this.loader.resources["bubbleTexture"].texture!)
+        this.pixi.stage.addChild(this.anotherBubble)
+        this.anotherBubble.x = Math.floor(Math.random() * 800)
+        this.anotherBubble.y = Math.floor(Math.random() * 400)
+
+        // adds animation
+        this.pixi.ticker.add((delta) => this.update(delta))
+    }
+
+    update(delta : number) {
+        this.fish.x -= 1.5 // determines movement speed and direction 
+        this.anotherFish.x -= 1 // determines movement speed and direction 
+        this.bubble.y -= 1.5 // determines movement speed and direction 
+        this.anotherBubble.y -= 1 // determines movement speed and direction 
+    }
 }
+
+new Game()
